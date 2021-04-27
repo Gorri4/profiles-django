fac2390c5110bc7b57433103a485017518a29761
Gudmundur Orri Palsson <gudmundur@mussil.com>
2021-04-27 10:35:57 +0000

    Django backend to git

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,7 +85,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
